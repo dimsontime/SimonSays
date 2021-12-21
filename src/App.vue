6a687b62f6ee4474288c73e053dd5a6e3826a2d1
Dmitriy Sinelnikov <dmitriy.sinelnikoff@gmail.com>
2021-12-21 13:48:56 +0700

    interface mokup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,98 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="section section-1"></div>
+    <div class="section section-2"></div>
+    <div class="section section-3"></div>
+    <div class="section section-4"></div>
+
+    <div class="section section-result">
+      <div>
+        <div>Current points</div>
+        <div>123</div>
+      </div>
+      
+      <div>
+        <div>Record</div>
+        <div>123</div>
+      </div>
+
+      <div class="startGame">Start game</div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style lang="scss">
+body {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+
+  .section {
+    width: 20%;
+    height: 100%;
+
+    &-1 {
+      background: blue;
+    }
+
+    &-2 {
+      background: red;
+    }
+
+    &-3 {
+      background: green;
+    }
+
+    &-4 {
+      background: yellow;
+    }
+
+    &-1, &-2, &-3, &-4 {
+      opacity: .7;
+
+      &:hover {
+        opacity: 1;
+      }
+    }
+
+    &-result {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      align-items: center;
+    }
+  }
+
+  .startGame {
+    background: #bada55;
+
+    padding: 30px;
+    border-radius: 10px;
+    cursor: pointer;
+  }
 }
 </style>
