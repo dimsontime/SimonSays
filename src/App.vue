47c013f62a2e8b0a9e90432a3d586984c1beb421
Dmitriy Sinelnikov <dmitriy.sinelnikoff@gmail.com>
2021-12-23 16:41:07 +0700

    Result bar component, game start method, sector click method, bad highlight method

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,98 @@
 <template>
   <div id="app">
-    <div class="section section-1"></div>
-    <div class="section section-2"></div>
-    <div class="section section-3"></div>
-    <div class="section section-4"></div>
+    <div class="section section-1" v-on:click="sectorClick(1)"></div>
+    <div class="section section-2" v-on:click="sectorClick(2)"></div>
+    <div class="section section-3" v-on:click="sectorClick(3)"></div>
+    <div class="section section-4" v-on:click="sectorClick(4)"></div>
 
-    <div class="section section-result">
-      <div>
-        <div>Current points</div>
-        <div>123</div>
-      </div>
-      
-      <div>
-        <div>Record</div>
-        <div>123</div>
-      </div>
+    <ResultNav 
+      v-bind:currentScore="currentScore"
+      v-bind:recordScore="recordScore"
 
-      <div class="startGame">Start game</div>
-    </div>
+      @startButton="startGame"
+    />
   </div>
 </template>
 
 <script>
-
+import ResultNav from "@/components/RsultNav.vue";
 
 export default {
   name: 'App',
+  data() {
+    return {
+      targetSeq: [],
+      currentSeq: [],
+      currentScore: 0,
+      recordScore: 0,
+      steps: 1,
+      gameStarted: false
+    }
+  },
   components: {
+    ResultNav : ResultNav
+  },
+  methods: {
+    startGame() {
+      this.targetSeq = [];
+      this.steps = 1;
+      this.currentScore = 0;
+      this.currentSeq = [];
 
+      this.targetSeq.push(Math.floor(Math.random() * 4) + 1);
+
+      this.gameStarted = true;
+
+      this.hightligthSectors();
+    },
+    sectorClick(id) {
+      if (this.gameStarted) {
+        this.currentSeq.push(id);
+
+
+        if (this.currentSeq.length == this.targetSeq.length) {
+          this.nextStep();
+        }
+      } else {
+        this.startGame();
+      }
+    },
+    nextStep() {
+      this.steps = this.steps + 1;
+      this.targetSeq.push(Math.floor(Math.random() * 4) + 1);
+      this.currentSeq = [];
+
+      this.hightligthSectors();
+      console.log(this.steps, "next step");
+    },
+    hightligthSectors() {
+      let items = [];
+      let counter = 0;
+      function show() {
+        if (counter < items.length) {
+          setTimeout(function() {
+            // items[counter].classList.add('active');
+            // setTimeout( () => items[counter].classList.remove('active'), 1000);
+            document.getElementsByClassName(items[counter])[0].classList.add('active');
+            setTimeout( () => document.getElementsByClassName(items[counter])[0].classList.remove('active'), 1000);
+            console.log(counter);
+            counter++;
+            show();
+          }, 1000);
+        }
+      }
+
+      for (let i of this.targetSeq) {
+        items.push('section-'+i);
+
+        // for(let item of items){
+        //   item.classList.add('active');
+        //   item.classList.remove('active');
+        // }
+      }
+
+      show();
+    }
   }
 }
 </script>
@@ -72,27 +137,12 @@ body {
     }
 
     &-1, &-2, &-3, &-4 {
-      opacity: .7;
+      opacity: .5;
 
-      &:hover {
+      &:hover, &.active {
         opacity: 1;
       }
-    }
-
-    &-result {
-      display: flex;
-      flex-direction: column;
-      justify-content: space-around;
-      align-items: center;
-    }
-  }
-
-  .startGame {
-    background: #bada55;
-
-    padding: 30px;
-    border-radius: 10px;
-    cursor: pointer;
+    }  
   }
 }
 </style>
---
+++ src/components/RsultNav.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="section section-result">
+      <div>
+        <h3>Current points</h3>
+        <h4>{{currentScore}}</h4>
+      </div>
+      
+      <div>
+        <h3>Record</h3>
+        <h4>{{recordScore}}</h4>
+      </div>
+
+      <div class="startGame" v-on:click="$emit('startButton')">Start game</div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['currentScore', 'recordScore']
+}
+</script>
+
+<style scoped lang="scss">
+    .section-result {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+
+        .startGame {
+            background: #bada55;
+
+            padding: 30px;
+            border-radius: 10px;
+            cursor: pointer;
+        }
+    }
+</style>
